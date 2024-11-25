c64e2e014e2cdb5f2b6b44d9c465ea4212af6665
IrenaPavlikha <kotova.dream@gmail.com>
2024-11-25 22:43:39 +0200

    Update

--- pages/zamovlennya.php
+++ pages/zamovlennya.php
@@ -3,19 +3,24 @@ global $pdo;
 include('../includes/db.php');
 include('../includes/header.php');
 
-$query = 'SELECT * FROM ZAMOVLENNYA';
+// Запит для об'єднання таблиць ZAMOVLENNYA, APTEKA та POKUPEC
+$query = 'SELECT Z.ID_ZAMOVLENNYA, Z.DATE, A.NAME AS APTEKA, P.NAME AS POKUPEC
+          FROM ZAMOVLENNYA Z
+          JOIN APTEKA A ON Z.ID_APTEKA = A.ID_APTEKA
+          JOIN POKUPEC P ON Z.ID_POKUPEC = P.ID_POKUPEC';
+
 $stmt = $pdo->query($query);
 
 echo "<h2>Список замовлень</h2>";
 echo "<table>
-        <tr><th>ID</th><th>Дата</th><th>Клієнт</th><th>Сума</th></tr>";
+        <tr><th>ID</th><th>Дата</th><th>Аптека</th><th>Покупець</th></tr>";
 
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     echo "<tr>
             <td>{$row['ID_ZAMOVLENNYA']}</td>
             <td>{$row['DATE']}</td>
-            <td>{$row['ID_POKUPEC']}</td>
-            <td>{$row['TOTAL_SUM']}</td>
+            <td>{$row['APTEKA']}</td>
+            <td>{$row['POKUPEC']}</td>
           </tr>";
 }
 echo "</table>";
